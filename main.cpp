2351e64b7ed0a60473ebbf30fab99a9daf7773d7
pcorwin <phoebe.corwin23@gmail.com>
2023-02-28 20:08:36 -0500

    Modified stackDriver function to highlight Stack functionality

--- main.cpp
+++ main.cpp
@@ -3,12 +3,19 @@
 
 int stackDriver(){
     Stack<int> int_stack;
+    cout<<"Stack Capacity: "<< int_stack.capacity() << endl;
     for (int i = 10; i > 0; i--) {
         int_stack.push(i);
+        if (int_stack.size()%3==0){
+            cout<<"Stack Size: "<< int_stack.size()<<endl;
+        }
     }
     int_stack.topElement();
     while (!int_stack.isEmpty()){
         int_stack.pop();
+        if (int_stack.size()%2==0){
+            cout<<"Stack Size: "<< int_stack.size()<<endl;
+        }
     }
     return 0;
 }
